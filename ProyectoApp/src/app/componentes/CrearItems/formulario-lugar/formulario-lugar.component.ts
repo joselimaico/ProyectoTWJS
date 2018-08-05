3010eae14bcebc77999e8f27affb2304086962a2
AndresSal <andres.salazar01@epn.edu.ec>
2018-08-04 19:27:37 -0500

    cambios: formulario lugar/habitacion - estructura logica del componente

--- ProyectoApp/src/app/componentes/CrearItems/formulario-lugar/formulario-lugar.component.ts
+++ ProyectoApp/src/app/componentes/CrearItems/formulario-lugar/formulario-lugar.component.ts
@@ -7,9 +7,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioLugarComponent implements OnInit {
 
+  inputNombreLugar: string;
+  inputTipoLugar: string;
+  inputSectorLugar: string;
+  inputDescripcionLugar: string;
+
+  inputNombreHabitacion: string;
+  inputTamanioHabitacion: string;
+  inputAmbienteHabitacion: string;
+  inputLuzSolar: string;
+
+  opcionesTipoLugar = [
+    'Oficina',
+    'Departamento',
+    'Jardín',
+    'Hogar'
+  ];
+
+  opcionesSectorLugar =[
+    'Norte',
+    'Centro-Norte',
+    'Centro',
+    'Centro-Sur',
+    'Sur'
+  ];
+
+  opcionesTamanioHabitacion = [
+    'Extra Grande',
+    'Grande',
+    'Mediana',
+    'Pequeña'
+  ];
+
+  opcionesAmbienteHabitacion = [
+    'Húmeda',
+    'Cálida',
+    'Seca'
+  ];
+
+  opcionesLuzSolar = [
+    'Si',
+    'No'
+  ];
+
+  urlImagenLugar = 'https://media-cdn.tripadvisor.com/media/photo-s/0c/11/0b/bb/lugar-dos-afectos.jpg';
+  urlImagenHabitacion = 'http://albergueoabeiro.com/wp-content/uploads/2016/04/habitacion-cama-de-matrimonio.jpg';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  crearNuevoRegistro(){
+    let bodyLugar = this.crearNuevoLugar();
+    let bodyHabitacion = this.crearNuevaHabitacion();
+    if (bodyLugar != null){
+    // AQUI LA CONSULTA AL SERVIDOR USANDO REQUEST EXCLUSIVO DEL LUGAR
+    }
+    if(bodyHabitacion != null){
+    // AQUI LA CONSULTA AL SERVIDOR USANDO REQUEST EXCLUSIVO DE LA HABITACIÓN
+    }
+
+  }
+
+
+  crearNuevaHabitacion(){
+    let luzSolar: boolean;
+    if(this.inputLuzSolar === 'Si'){
+      luzSolar = true;
+    }
+    if(this.inputLuzSolar === 'No'){
+      luzSolar = false;
+    }
+    let body = {
+      nombreHabitacion: `${this.inputNombreHabitacion}`,
+      tamanioHabitacion: `${this.inputTamanioHabitacion}`,
+      luzSolarHabitacion: `${luzSolar}`,
+      descripcionHabitacion: `${this.inputAmbienteHabitacion}`,
+      imagenHabitacion: `${this.urlImagenHabitacion}`
+    };
+    console.log('objeto JSON', body);
+    return body;
+  }
+
+
+  crearNuevoLugar(){
+    let body = {
+      nombreLugar: `${this.inputNombreLugar}`,
+      tipoLugar: `${this.inputTipoLugar}`,
+      sectorLugar: `${this.inputSectorLugar}`,
+      descripcionLugar: `${this.inputDescripcionLugar}`,
+      imagenLugar: `${this.urlImagenLugar}`
+    };
+    console.log('objeto JSON', body);
+    return body;
   }
 
 }
